2b4f8c6134dfbc6501bf5906f8d7db990b707ade
Vaneet Chanana <vaneetchanana98@gmail.com>
2022-09-18 01:21:01 +0530

    Updates

---
+++ precache-manifest.a8ee7c9d993f1816900791953869a9af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba18772b768a6de5619ee82cabcd0b0",
+    "url": "/snapshot/index.html"
+  },
+  {
+    "revision": "756e07dbcefc9038fbdc",
+    "url": "/snapshot/static/css/main.1b83491f.chunk.css"
+  },
+  {
+    "revision": "cf5b7894a53918e8b0ac",
+    "url": "/snapshot/static/js/2.f587ad51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/snapshot/static/js/2.f587ad51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756e07dbcefc9038fbdc",
+    "url": "/snapshot/static/js/main.3e92b865.chunk.js"
+  },
+  {
+    "revision": "4d6d0105be043c70fb56",
+    "url": "/snapshot/static/js/runtime-main.5d3f71fe.js"
+  }
+]);
